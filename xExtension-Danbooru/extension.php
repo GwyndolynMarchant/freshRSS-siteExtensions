6ea0b68903ebf62c1ecc99ab5ed886e662a115a9
Gwyndolyn Marchant <luna@shadenexus.com>
2023-07-18 19:01:23 -0400

    try

--- xExtension-Danbooru/extension.php
+++ xExtension-Danbooru/extension.php
@@ -2,35 +2,26 @@
 
 class DanbooruExtension extends Minz_Extension {
 
-	function debug_to_console($data) {
-	    $output = $data;
-	    if (is_array($output))
-	        $output = implode(',', $output);
-
-	    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-	}
-
     protected function supports($entry)
     {
-    	debug_to_console("checking if the url is supported");
         return (stripos($entry->link(), '://danbooru.donmai.us/posts') !== false);
     }
 
 
 	public function init(): void {
         $this->registerHook('entry_before_insert', array($this, 'danbooruFix'));
-        //$this->registerHook('entry_before_display', array($this, 'danbooruFix'));
 	}
 
 	public function danbooruFix(FreshRSS_Entry $entry): FreshRSS_Entry {
 		if (!$this->supports($entry)) { return $entry; }
-		debug_to_console("made it this far");
 
 		preg_match("/<p>(.*)<\/p>/", $entry->content(), $matches);
 		$entry->_tags(explode(" ", $matches[1]));
 		
-		$dom = new DOMDocument;
+		libxml_use_internal_errors(true);
+        $dom = new DOMDocument;
         $dom->loadHTMLFile($entry->link());
+        libxml_use_internal_errors(false);
         $xpath = new DOMXpath($dom);
         $content = $xpath->query("//section[id=\"content\"]");
 
