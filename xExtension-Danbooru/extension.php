9ef35faddd9f503f98053eff4b0c1dd4e342032b
Gwyndolyn Marchant <luna@shadenexus.com>
2023-07-18 19:10:19 -0400

    try

--- xExtension-Danbooru/extension.php
+++ xExtension-Danbooru/extension.php
@@ -51,41 +51,14 @@ class DanbooruExtension extends Minz_Extension {
         libxml_use_internal_errors(false);
         $xpath = new DOMXpath($dom);
 
-        $comicContainer = $xpath->query("//div[contains(@class, 'comic-item-container')]");
+        $comicContainer = $xpath->query("//section[@id='content']");
 
         if (!is_null($comicContainer)) {
             $comicContainer = $comicContainer->item(0);
-
             $originalHash = $entry->hash();
 
-            // add some meta info
-            $entry->_author($comicContainer->getAttribute('data-creator'));
-            $entry->_title($entry->title() . ' - ' . $comicContainer->getAttribute('data-title'));
-
-            // Lets not only focus on the image, but keep the link to the original page as well.
-            // This is only fair for Scott, who brings us these famous comic strips since so many years!
-            $comicLinks = $xpath->query("//a[@class='img-comic-link']", $comicContainer);
-            if (!is_null($comicLinks)) {
-                $node = $comicLinks->item(0);
-
-                // the image URL is now started protocol relative, so we need to prepend https, otherwise
-                // reading the articles via the API might fail. In a browser you will not see the problem, only
-                // when articles are displayed inside an app that does not support these protocol-less URLs.
-                $image = $xpath->query("//img", $node);
-                if (!is_null($image)) {
-                    $image = $image->item(0);
-                    if (!is_null($image)) {
-                        $source = $image->getAttribute('src');
-                        if (strpos($source, '//') === 0) {
-                            $image->setAttribute('src', 'https:' . $source);
-                        }
-                    }
-                }
-
-                $iconWithLink = $node->ownerDocument->saveHTML($node);
-                $entry->_content($iconWithLink);
-            }
-
+            $iconWithLink = $comicContainer->ownerDocument->saveHTML($node);
+            $entry->_content($iconWithLink);
             $entry->_hash($originalHash);
         }
 
