a4f18c61aa754cc444bbee189152e18dfdc7b88c
Gwyndolyn Marchant <luna@shadenexus.com>
2023-07-21 11:26:07 -0400

    tag list is working

--- xExtension-Danbooru/extension.php
+++ xExtension-Danbooru/extension.php
@@ -27,12 +27,14 @@ class DanbooruExtension extends Minz_Extension {
         libxml_use_internal_errors(false);
         if ($dom === false) { $html = "Couldn't load DOM."; }
         else {
-        	$html = $dom->saveHTML($dom->getElementById("content"));
+        	$content = $dom->getElementById("content");
+        	if ($content === null) { $html = "Could not find content in DOM"; }
+        	else { $html = $dom->saveHTML($dom->getElementById("content")); }
     	}
 
         // Setting the content to the original post + the scraped page content - this is so we have video previews
         $originalHash = $entry->hash();
-        $entry->_content($entry->content() . '\n' . $html);
+        $entry->_content($entry->content() . '</br>' . $html);
         $entry->_hash($originalHash);
 
 		return $entry;
