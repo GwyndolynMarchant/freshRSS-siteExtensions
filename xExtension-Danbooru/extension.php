baf67dc97dc71a83cfd976eb38fa07b9e1e25b4c
Gwyndolyn Marchant <luna@shadenexus.com>
2023-07-21 10:58:24 -0400

    try

--- xExtension-Danbooru/extension.php
+++ xExtension-Danbooru/extension.php
@@ -18,21 +18,17 @@ class DanbooruExtension extends Minz_Extension {
 		preg_match("/<p>(.*)<\/p>/", $entry->content(), $matches);
 		$entry->_tags(explode(" ", $matches[1]));
 		
+		$html = "Danbooru page recognized but loading failed.";
+
 		// Load the actual page
 		libxml_use_internal_errors(true);
         $dom = new DOMDocument;
         $dom->loadHTMLFile($entry->link());
         libxml_use_internal_errors(false);
-        $xpath = new DOMXpath($dom);
-
-        $html = "Couldn't load page";
-        $container = $xpath->query("//section[@id='content']");
-        if (!is_null($container)) {
-        	$content = $container->item(0);
-        	if ($content !== null) {
-                $html = $content->ownerDocument->saveHTML($node);
-            }
-        }
+        if ($dom === false) { $html = "Couldn't load DOM."; }
+        else {
+        	$html = $dom->saveHTML($dom->getElementById("content"));
+    	}
 
         // Setting the content to the original post + the scraped page content - this is so we have video previews
         $originalHash = $entry->hash();
