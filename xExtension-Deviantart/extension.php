d7bea72dd342aa98686b02e5374cab2be83c8171
Gwyndolyn Marchant <luna@shadenexus.com>
2023-07-21 20:53:44 -0400

    url transform working

--- xExtension-Deviantart/extension.php
+++ xExtension-Deviantart/extension.php
@@ -32,11 +32,6 @@ class DeviantartExtension extends Minz_Extension {
 			$rem->parentNode->removeChild($rem);
 		}
 
-		foreach ($xpath->query('//img[contains(@class, "enclosure-thumbnail")]', null, false) as $node) { $elementsToRemove[] = $node; }
-		foreach ($elementsToRemove as $rem) {
-			$rem->parentNode->removeChild($rem);
-		}
-
 		// Make the new post
 		$originalHash = $entry->hash();
 		$entry->_content($dom->saveHTML());
